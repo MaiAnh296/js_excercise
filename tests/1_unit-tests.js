f3b3421e18dd20e9d9b2ffe222a81da22affafb4
PYCO\anh.maithilan <anhmtl1@gmail.com>
2025-01-30 19:19:45 -0500

    changed

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -172,7 +172,7 @@ suite("Unit Tests", function () {
     /** 10 - Use assert.approximately() to make the tests pass. **/
     test("#approximately", function () {
       assert.approximately(weirdNumbers(0.5), 1, 0.5);
-      assert.approximately(weirdNumbers(0.2), 1, 0.2);
+      assert.approximately(weirdNumbers(0.2), 1, 0.8);
    });
   });
 
