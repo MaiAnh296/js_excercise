ec003557885ebb9cafc51c6bdac6232f557c5b2b
PYCO\anh.maithilan <anhmtl1@gmail.com>
2025-01-30 22:38:14 -0500

    #12

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -178,9 +178,9 @@ suite("Unit Tests", function () {
 
   // // -----------------------------------------------------------------------------
 
-  // const winterMonths = ["dec,", "jan", "feb", "mar"];
-  // const backendLanguages = ["php", "python", "javascript", "ruby", "asp"];
-  // suite("Arrays", function () {
+  const winterMonths = ["dec,", "jan", "feb", "mar"];
+  const backendLanguages = ["php", "python", "javascript", "ruby", "asp"];
+   suite("Arrays", function () {
   //   // #11
   //   test("#isArray, #isNotArray", function () {
   //     assert.fail(
@@ -189,12 +189,24 @@ suite("Unit Tests", function () {
   //     );
   //     assert.fail([1, 2, 3].indexOf(2), "indexOf returns a number");
   //   });
+
+    /** 11 - Use assert.isArray() or assert.isNotArray() to make the tests pass. **/
+    test("#isArray, #isNotArray", function () {
+      assert.isArray("isThisAnArray?".split(""), "String.prototype.split() returns an array");
+      assert.isNotArray([1, 2, 3].indexOf(2), "indexOf returns a number");
+    });
   //   // #12
   //   test("Array #include, #notInclude", function () {
   //     assert.fail(winterMonths, "jul", "It's summer in july...");
   //     assert.fail(backendLanguages, "javascript", "JS is a backend language");
   //   });
-  // });
+
+    /** 12 - Test the arrays winterMonths and backendLanguages. **/
+    test("Array #include, #notInclude", function () {
+      assert.notInclude(winterMonths, "jul", "It's summer in july...");
+      assert.include(backendLanguages, "javascript", "JS is a backend language");
+    });
+   });
 
   // // -----------------------------------------------------------------------------
 
